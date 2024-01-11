4aab5c2944ff355cf28099f5299aae1ea49a90fa
Konstantin Chernyshev <k4black@ya.ru>
2024-01-11 09:42:46 +0100

    refactor: move flow in separate bench

--- fast_aug/benches/text.rs
+++ fast_aug/benches/text.rs
@@ -1,10 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, SamplingMode};
 use fast_aug::text::*;
-use fast_aug::flow::*;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::sync::Arc;
 use rand::SeedableRng;
 use fast_aug::base::BaseAugmenter;
 
@@ -21,13 +19,13 @@ fn load_txt_to_string_vector<P: AsRef<Path>>(path: P) -> io::Result<Vec<String>>
 }
 
 
-fn benchmark_individual_augmenters(c: &mut Criterion) {
+fn benchmark_text(c: &mut Criterion) {
     // let mut rng = rand::thread_rng();
     let mut rng = rand::rngs::SmallRng::from_entropy();
 
     // Load dataset
     let text_data = load_txt_to_string_vector(BENCHMARK_DATASET_PATH).expect("Unable to load dataset");
-    let text_data = text_data[0..text_data.len()/2].to_vec();
+    let text_data = text_data[0..text_data.len()/4].to_vec();
 
     // Benchmark for RandomWordsAugmenter
     let mut group = c.benchmark_group("RandomWordsAugmenter");
@@ -92,111 +90,14 @@ fn benchmark_individual_augmenters(c: &mut Criterion) {
     // Add similar sections here for RandomCharsAugmenter, etc.
 }
 
-fn benchmark_flow(c: &mut Criterion) {
-    // let mut rng = rand::thread_rng();
-    let mut rng = rand::rngs::SmallRng::from_entropy();
-    // let mut rng = rand::rngs::SmallRng::from_seed([0; 32]);
-
-
-    // Load dataset
-    let text_data = load_txt_to_string_vector(BENCHMARK_DATASET_PATH).expect("Unable to load dataset");
-    let text_data = text_data[0..text_data.len()/2].to_vec();
-
-    // Benchmark pipelines/flow
-    let mut group = c.benchmark_group("Flow");
-    group.sampling_mode(SamplingMode::Flat);
-    group.bench_function("SequentialAugmenter", |b| {
-        b.iter(|| {
-            let aug_1 = RandomWordsAugmenter::new(
-                TextAction::Swap,
-                TextAugmentParameters::new(0.5, None, None),
-                None,
-            );
-            let aug_2 = RandomCharsAugmenter::new(
-                TextAction::Swap,
-                TextAugmentParameters::new(0.5, Some(2), Some(5)),
-                TextAugmentParameters::new(0.5, None, None),
-                None,
-            );
-            let aug_3 = RandomWordsAugmenter::new(
-                TextAction::Delete,
-                TextAugmentParameters::new(0.5, None, None),
-                None,
-            );
-            let aug_4 = RandomCharsAugmenter::new(
-                TextAction::Delete,
-                TextAugmentParameters::new(0.5, None, None),
-                TextAugmentParameters::new(0.5, Some(1), Some(2)),
-                None,
-            );
-            let pipeline = SequentialAugmenter::new(vec![
-                Arc::new(aug_1),
-                Arc::new(aug_2),
-                Arc::new(aug_3),
-                Arc::new(aug_4),
-            ]);
-            for text in text_data.iter() {
-                black_box(pipeline.augment(text.clone(), &mut rng));
-            }
-        });
-    });
-    group.bench_function("SelectorAugmenter", |b| {
-        b.iter(|| {
-            let aug_1 = RandomWordsAugmenter::new(
-                TextAction::Swap,
-                TextAugmentParameters::new(0.5, None, None),
-                None,
-            );
-            let aug_2 = RandomCharsAugmenter::new(
-                TextAction::Swap,
-                TextAugmentParameters::new(0.5, Some(2), Some(5)),
-                TextAugmentParameters::new(0.5, None, None),
-                None,
-            );
-            let aug_3 = RandomWordsAugmenter::new(
-                TextAction::Delete,
-                TextAugmentParameters::new(0.5, None, None),
-                None,
-            );
-            let aug_4 = RandomCharsAugmenter::new(
-                TextAction::Delete,
-                TextAugmentParameters::new(0.5, None, None),
-                TextAugmentParameters::new(0.5, Some(1), Some(2)),
-                None,
-            );
-            let pipeline = SelectorAugmenter::new(vec![
-                Arc::new(aug_1),
-                Arc::new(aug_2),
-                Arc::new(aug_3),
-                Arc::new(aug_4),
-            ], None);
-            for text in text_data.iter() {
-                black_box(pipeline.augment(text.clone(), &mut rng));
-            }
-        });
-    });
-    group.bench_function("ChanceAugmenter", |b| {
-        b.iter(|| {
-            let aug = RandomWordsAugmenter::new(
-                TextAction::Swap,
-                TextAugmentParameters::new(0.5, None, None),
-                None,
-            );
-            let pipeline = ChanceAugmenter::new(Arc::new(aug), 0.5);
-            for text in text_data.iter() {
-                black_box(pipeline.augment(text.clone(), &mut rng));
-            }
-        });
-    });
-}
 
 // Define the groups using the functions
 criterion_group!{
     name = benches;
     config = Criterion::default()
-        .sample_size(10)
-        .measurement_time(std::time::Duration::from_secs(20))
+        .sample_size(20)
+        .measurement_time(std::time::Duration::from_secs(30))
         .significance_level(0.01);
-    targets = benchmark_individual_augmenters, benchmark_flow
+    targets = benchmark_text
 }
 criterion_main!(benches);
